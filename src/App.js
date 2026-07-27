dde67d2da6d85be1792c11eee19b6167d8124ae9
Nəzər <n1e2z3e4r5@gmail.com>
2026-07-27 15:16:38 +0400

    feat: add fake data

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <Header />
-
+    
       <Outlet />
     </div>
   );
--- src/shop/CardSection.jsx
+++ src/shop/CardSection.jsx
@@ -1,7 +1,45 @@
+import Img1 from "../assets/image 1.png";
+import Img2 from "../assets/image 2.svg";
+import Img3 from "../assets/image 3.svg";
+import Img4 from "../assets/image 4.png";
+import Img5 from "../assets/image 5.png";
+import Img6 from "../assets/image 6.svg";
+
 function CardSection() {
-return <div>
-    
-  </div>;
+  return <div></div>;
 }
 
 export default CardSection;
+
+export const data = [
+  {
+    name: "Banana",
+    price: 1.32,
+    img: Img1,
+  },
+  {
+    name: "Cabbage",
+    price: 0.82,
+    img: Img2,
+  },
+  {
+    name: "Tomatto",
+    price: 1.23,
+    img: Img3,
+  },
+  {
+    name: "Egg plant",
+    price: 0.82,
+    img: Img4,
+  },
+  {
+    name: "Lettuce",
+    price: 1.02,
+    img: Img5,
+  },
+  {
+    name: "Pottato",
+    price: 2.3,
+    img: Img6,
+  },
+];
